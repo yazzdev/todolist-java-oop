3530b3edf333f4fbd99640c0a2c5dafd09e60c36
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2024-12-23 13:29:06 +0700

    add Class Todo in package entity

---
+++ src/entity/Todo.java
@@ -0,0 +1,20 @@
+package entity;
+
+public class Todo {
+  private String todo;
+
+  public Todo() {
+  }
+
+  public Todo(String todo) {
+    this.todo = todo;
+  }
+
+  public String getTodo() {
+    return todo;
+  }
+
+  public void setTodo(String todo) {
+    this.todo = todo;
+  }
+}
